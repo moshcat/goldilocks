84d2b45b9c4005b6477f7f8557452f6c07243a9a
moshcat <mauricecathoverile@gmail.com>
2025-02-12 23:49:24 +0800

    feat(main): change text-color on all pages

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <div class="flex flex-col min-h-screen font-Figtree bg-goldi-bg">
+  <div class="flex flex-col min-h-screen font-Figtree bg-goldi-bg text-white">
     <RouterView />
   </div>
 </template>
